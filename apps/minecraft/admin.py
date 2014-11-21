f900a5e036176357c6d8b83b9f008daceb34eabd
Julian Wachholz <julian@wachholz.ch>
2014-11-21 15:17:02 +0100

    Model admin for webop logged actions

--- apps/minecraft/admin.py
+++ apps/minecraft/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth.forms import UserChangeForm
-from .models import WebOperator
+from .models import WebOperator, LogAction
+
+
+class LogActionInline(admin.TabularInline):
+    model = LogAction
+    extra = 0
 
 
 @admin.register(WebOperator)
@@ -8,3 +13,11 @@ class WebOperatorAdmin(admin.ModelAdmin):
     list_display = ['user', 'gamemode', 'get_coords']
     search_fields = ['user', 'user__username']
     ordering = ['user__username']
+    inlines = [LogActionInline]
+
+
+@admin.register(LogAction)
+class LogActionAdmin(admin.ModelAdmin):
+    list_display = ['timestamp', 'webop', 'action', 'arguments']
+    search_fields = ['webop', 'action', 'arguments']
+    date_hierarchy = 'timestamp'
